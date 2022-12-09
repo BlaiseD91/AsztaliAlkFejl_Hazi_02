01c752dd242877b838d5d9260a76856403fe7079
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-09 12:48:25 +0100

    Projekt létrehozása

---
+++ 05/Csere/src/main/java/com/mycompany/csere/Csere.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.csere;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-09
+ * 
+ * Cseréljük ki két változó értékét! Oldjuk meg úgy, hogy ne használjunk segédváltozót!
+ */
+public class Csere {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        
+        System.out.println("a változó értéke: " + a);
+        System.out.println("b változó értéke: " + b);
+    }
+}
