3b3bf62a49f12d74935777b871d1899b2ade216c
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-09 12:24:58 +0100

    nem használt import eltávolítva

--- 04/elteltNapok/src/main/java/com/mycompany/elteltnapok/ElteltNapok.java
+++ 04/elteltNapok/src/main/java/com/mycompany/elteltnapok/ElteltNapok.java
@@ -14,8 +14,6 @@ package com.mycompany.elteltnapok;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.lang.NumberFormatException;
 public class ElteltNapok {
 
     public static void main(String[] args) throws IOException{
