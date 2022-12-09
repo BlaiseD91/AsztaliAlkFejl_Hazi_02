3191d8adc787a7c53868d6e0ffdef16a646de5d5
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-09 13:06:47 +0100

    Projekt létrehozása

---
+++ 14/Nagyobb/src/main/java/com/mycompany/nagyobb/Nagyobb.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.nagyobb;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-09
+ * 
+ * Írd ki, hogy melyik a legnagyobb szám a [100;100000] intervallumból, amelyiknek az utolsó számjegye nagyobb, mint az előtte lévő számjegyek összege.
+ */
+public class Nagyobb {
+
+    public static void main(String[] args) {
+        
+    }
+}
