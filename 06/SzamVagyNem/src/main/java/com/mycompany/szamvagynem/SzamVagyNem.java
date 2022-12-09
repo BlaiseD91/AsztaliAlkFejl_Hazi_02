b7c4e7ccd0fdb459aa800f03cae33ae0b8bf4328
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-09 12:00:52 +0100

    kezdés

---
+++ 06/SzamVagyNem/src/main/java/com/mycompany/szamvagynem/SzamVagyNem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.szamvagynem;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-09
+ * 
+ * Vizsgáljuk meg, hogy egy beolvasott karakterlánc első karaktere szám-e. A maradék karakterekből írjunk ki annyit amennyit az első karakter jelez.
+ */
+public class SzamVagyNem {
+
+    public static void main(String[] args) {
+    }
+}
