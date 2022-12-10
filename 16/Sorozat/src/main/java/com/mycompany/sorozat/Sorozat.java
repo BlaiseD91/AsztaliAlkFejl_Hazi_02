3a69810e5653c30d4122edb35d0982d3a4266890
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-10 22:06:37 +0100

    Projekt létrehozása

---
+++ 16/Sorozat/src/main/java/com/mycompany/sorozat/Sorozat.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sorozat;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-10
+ * 
+ * Írd ki annak a sorozatnak a 15. elemét, amelyet úgy kapsz meg, hogy minden következő elemet az előző szám számjegyeinek kétszereséből állítod elő! (1, 2, 4, 8, 16, 212, 424, 848, 16816)
+ */
+public class Sorozat {
+
+    public static void main(String[] args) {
+        
+    }
+}
