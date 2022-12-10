43d1763f42739b996e5003a789f88a4266993c84
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-10 20:42:47 +0100

    Kész, teszt hozzáadva

--- 18/ParosParatlan/src/main/java/com/mycompany/parosparatlan/ParosParatlan.java
+++ 18/ParosParatlan/src/main/java/com/mycompany/parosparatlan/ParosParatlan.java
@@ -12,17 +12,23 @@ package com.mycompany.parosparatlan;
  * 
  * Írjunk függvényt, amely igaz értéked ad vissza, ha a paraméterként megadott szám páratlan, egyéb esetben hamis értéket!
  */
-
+import java.io.*;
 
 public class ParosParatlan {
 
-    public static void main(String[] args) {
-        int szam;
+    public static void main(String[] args) throws IOException {
         
+        //paratlan teszt
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Adj meg egy számot: ");
+        if(paratlan(Integer.parseInt(br.readLine())))
+            System.out.println("A szám páratlan");
+        else
+            System.out.println("A szám páros");
     }
     
     public static boolean paratlan(int szam){
-        if (szam % 2 == 1)
+        if(szam % 2 != 0)
             return true;
         else
             return false;
