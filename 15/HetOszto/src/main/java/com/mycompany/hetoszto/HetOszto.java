860bde7305b5cfe98203462f4ae878abb78445df
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-10 21:11:54 +0100

    Projekt létrehozáa

---
+++ 15/HetOszto/src/main/java/com/mycompany/hetoszto/HetOszto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hetoszto;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-10
+ * 
+ * Írd ki 100-tól kezdve a második 10 darab olyan számot, amelyiknek pontosan 7 osztója van (1-et és önmagát figyelmen kívül hagyva).
+ */
+public class HetOszto {
+
+    public static void main(String[] args) {
+        
+    }
+}
