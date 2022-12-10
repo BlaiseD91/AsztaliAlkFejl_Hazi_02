5855f674431e3e3953479655495a709f799704d6
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-10 20:29:19 +0100

    Projekt létrehozása

---
+++ 18/ParosParatlan/src/main/java/com/mycompany/parosparatlan/ParosParatlan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.parosparatlan;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-10
+ * 
+ * Írjunk függvényt, amely igaz értéked ad vissza, ha a paraméterként megadott szám páratlan, egyéb esetben hamis értéket!
+ */
+public class ParosParatlan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
