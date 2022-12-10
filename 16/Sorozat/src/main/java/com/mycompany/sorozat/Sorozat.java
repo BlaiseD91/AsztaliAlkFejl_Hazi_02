3633d11e0ae7bd8483872b724e19931e00c113c1
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-10 23:22:39 +0100

    kész

--- 16/Sorozat/src/main/java/com/mycompany/sorozat/Sorozat.java
+++ 16/Sorozat/src/main/java/com/mycompany/sorozat/Sorozat.java
@@ -15,6 +15,30 @@ package com.mycompany.sorozat;
 public class Sorozat {
 
     public static void main(String[] args) {
+        final int DB_SZAM =  13; //elemszám, ha később több kellene
+        long[] szamok = new long[DB_SZAM]; //itt tároljuk a találatokat
+        long szam = 1; //1-től kezdünk
+        int megvanDb = 0;
         
+        //számok előállítása
+        szamok[megvanDb] = szam;
+        megvanDb++;
+        do {
+            String szamStr = Long.toString(szam);
+            int hossz = szamStr.length();
+            String segedStr = "";
+            long segedSzam;
+            for (int i = 0; i < hossz; i++) {
+                segedSzam = 2 * ((long) szamStr.charAt(i)-48);
+                segedStr = segedStr + String.valueOf(segedSzam);
+            }
+            szam = Long.parseLong(segedStr);
+
+            szamok[megvanDb] = szam;
+            megvanDb++;
+        }while(megvanDb != DB_SZAM);
+        
+        //Kiíratás
+        System.out.println(szamok[szamok.length-1]);
     }
 }
