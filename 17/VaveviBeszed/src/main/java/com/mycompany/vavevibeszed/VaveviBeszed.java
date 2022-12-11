fca36001ba35cadc03d96b2f868bcd25cff748bf
Székely Balázs Csaba <szekelyb91@gmail.com>
2022-12-11 16:41:57 +0100

    Projekt létrehozása

---
+++ 17/VaveviBeszed/src/main/java/com/mycompany/vavevibeszed/VaveviBeszed.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.vavevibeszed;
+
+/**
+ *
+ * @author Székely Balázs Csaba
+ * @date 2022-12-11
+ * 
+ * Írjunk programot, amely beolvas egy ékezetes karakterek nélküli mondatot és kiírja a következőképen átalakítva: a szöveg magánhangzóit lecseréli az alábbi módon:
+ *   a -> ava, e -> eve, i -> ivi, o -> ovo, u -> uvu
+ *   Például: Ma szep idonk van. Visszaírja: Mava szevep ividovo vavan.
+ */
+public class VaveviBeszed {
+
+    public static void main(String[] args) {
+        
+    }
+}
